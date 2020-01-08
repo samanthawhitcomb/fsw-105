e908b97ab4e5e2d2ebf68971c05790dad9780d78
Sami <Samantha.whitcomb@bryanuniversity.edu>
2020-01-08 12:52:04 -0800

    Node Modules

---
+++ week4/app.js
@@ -0,0 +1,5 @@
+const node = require('./node')
+console.log(node.sum(2, 6));
+console.log(node.sum(node.PI, 6));
+console.log(node.PI);
+console.log(node.func(['Ben', 'Jessica','Bobby','Jill']));
---
+++ week4/node.js
@@ -0,0 +1,8 @@
+module.exports.sum = (num1, num2) => num1 + num2;
+module.exports.PI = 3.14;
+module.exports.func = function(arr){
+    return 'There are ' + arr.length + ' elements in this array';
+}
+        
+
+
