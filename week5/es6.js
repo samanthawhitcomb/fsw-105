934e54dfe7770aaf64c4efb8d9da2670b6a2e3c0
Sami <Samantha.whitcomb@bryanuniversity.edu>
2020-01-20 13:38:36 -0800

    ES6 part 2

---
+++ week5/es6.js
@@ -0,0 +1,116 @@
+// let name1 = "John"
+// let age = 101
+// function runForLoop(pets) {
+//     const petObjects = []
+//     for (let i = 0; i < pets.length; i++){
+//         let pet = { type: pets[i] }
+//         if (pets[i] === "cat") {
+//             name = "fluffy"
+//         } else {
+//             name = "spot"
+//         } 
+//         console.log("pet name: ", name)
+//         pet.name = name
+//         petObjects.push(pet)
+//     }
+//     console.log("man name: ", name1)
+//     return petObjects
+// }
+// runForLoop(["cat", "dog"])
+
+
+//Re-write this .map() using an arrow function:
+
+//Be aware that if JavaScript sees a { directly after the => it will think it's starting a function, and not starting an object, so the : will be an unexpected symbol.
+
+// const arr = ["bright orange", "ripe", "rotten"]
+
+// const result = arr.map(carrot => ({type: "carrot", name: carrot}))
+
+// console.log(result)
+
+// //-write this .filter() using an arrow function:
+// const people = [
+//     {
+//         name: "Princess Peach",
+//         friendly: false
+//     },
+//     {
+//         name: "Luigi",
+//         friendly: true
+//     },
+//     {
+//         name: "Mario",
+//         friendly: true
+//     },
+//     {
+//         name: "Bowser",
+//         friendly: false
+//     }
+// ]
+
+// const filterForFriendly = people.filter(person => person.friendly === true)
+// console.log(filterForFriendly)
+
+//Re-write the following functions to be arrow functions:
+// let doMath = [20, 5]
+
+// const result = doMath.reduce((a, b) => a + b)
+
+// console.log(result)
+
+// let doMath = [20, 5]
+
+// const result = doMath.reduce((a, b) => a * b)
+
+// console.log(result)
+
+//Write a printString function that takes firstName, lastName, and age as parameters and returns a string like the following:
+//firstName should default to "Jane"
+//lastName should default to "Doe"
+//age should default to 100
+
+
+//Use template literals to build the string from #5
+// var p = {
+//     name: `Jane`,
+//     lastName: `Doe`,
+//     age: 100,
+// };
+// console.log('Hi ' + p.name + ' ' + p.lastName + ', how does it feel to be ' + p.age + '?')
+
+//Use the shorthand syntax to make the following filter take up one line. Copy and paste the array to test it.
+
+// const animals = [
+//     {
+//         type: "dog",
+//         name: "theodore"
+//     },
+//     {
+//         type: "cat",
+//         name: "whiskers"
+//     },
+//     {
+//         type: "pig",
+//         name: "piglette"
+//     },
+//     {
+//         type: "dog",
+//         name: "sparky"
+//     }
+// ];
+// const filterForDogs = animals.filter(animal => animal.type === "dog")
+// console.log(filterForDogs)
+
+//Using template literals, write a function that takes location and name parameters and outputs a message formatted like this:
+
+var loc = {
+    name: `Janice`,
+    location: `Hawaii`,
+    age: 100,
+};
+console.log(`Hi `  + loc.name +`!\n`,
+
+`Welcome to ` +loc.location+`.\n`,
+
+`I hope you enjoy your stay. Please ask the president of ` +loc.location+ ` if you need anything.`)
---
+++ week5/index.js
@@ -0,0 +1,69 @@
+// //1) Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
+// let vals = [3 ,6 ,8 ,2 ];
+
+// function fiveAndGreaterOnly(num){
+//     return (num >= 5)
+// }
+// vals = vals.filter(fiveAndGreaterOnly);
+// console.log(vals);  ///[6, 8]
+
+// //2) Given an array of numbers, return a new array that only includes the even numbers.
+// const arr = [3 ,6 ,8 ,2 ]
+
+// const evensOnly = arr.filter(function(num) {
+//     if (num % 2 === 0)
+//         return num
+// })
+// console.log(evensOnly)
+
+//3) Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
+// const str = ["by", "dog", "wolf", "family", "eaten", "camping"]
+
+// const fiveCharactersOrFewerOnly = str.filter(function(name) {
+//     if (name.length <= 5) {
+//         return name
+//     }
+// })
+// console.log(fiveCharactersOrFewerOnly)
+
+// //4) Given an array of people objects, return a new array that has filtered out all those who don't belong to the club.
+// const club = [
+//     {name: "Angelina Jolie", member: true },
+//     {name: "Eric Jones", member: false},
+//     {name: "Paris Hilton", member: true},
+//     {name: "Kayne West", member: false},
+//     {name: "Bob Ziroll", member: true}
+// ]
+// const peopleWhoBelongToTheIlluminati = club.filter(function(dontBelong) {
+//     if(dontBelong.member === true){
+//     return dontBelong
+//     }
+// })
+// console.log(peopleWhoBelongToTheIlluminati)
+
+// //5) Make a filtered list of all the people who are old enough to see The Matrix (younger than 18)
+// const ofAge = [
+//     {name: "Angelina Jolie", age: 80 },
+//     {name: "Eric Jones", age: 2},
+//     {name: "Paris Hilton", age: 5},
+//     {name: "Kayne West", age: 16},
+//     {name: "Bob Ziroll", age: 100}
+// ]
+// const oldEnough = ofAge.filter(function(older) {
+//     if(older.age > 18){
+//     return older
+//     }
+// })
+// console.log(oldEnough)
+//Use the built-in .map() method on arrays to solve all of these problems:
+//1) Make an array of numbers that are doubles of the first array
+// const arr = [2, 5, 100]
+
+// const result = arr.map(num => num * 2)
+// console.log(result)
+//2) Take an array of numbers and make them strings
+const arr = [2, 5, 100]
+
+const result = arr.map(function(string){
+    return 
+})
