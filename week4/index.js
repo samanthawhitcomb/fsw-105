8de0fc1d242442e77610451567d9da589a8ac826
Sami <Samantha.whitcomb@bryanuniversity.edu>
2020-01-27 17:59:38 -0800

    string and array updated

--- week4/index.js
+++ week4/index.js
@@ -1,24 +1,24 @@
 // //capitalize and lowercase
-// var name = "luke"
-// var uppercasedLuke = name.toUpperCase()
-// var lowercasedLuke = uppercasedLuke.toLowerCase()
-//     console.log(uppercasedLuke + lowercasedLuke)
+var name = "luke"
+var uppercasedLuke = name.toUpperCase()
+var lowercasedLuke = uppercasedLuke.toLowerCase()
+    console.log(uppercasedLuke + lowercasedLuke)
 // //function that returns a number half the length, and rounded down.
 
-// var string = "hello" // => 2
-// console.log(Math.floor(string.length / 2));
+var string = "hello" // => 2
+console.log(Math.floor(string.length / 2));
 
-// var string = "hello World" // => 5
-// console.log(Math.floor(string.length / 2));
+var string = "hello World" // => 5
+console.log(Math.floor(string.length / 2));
 
 // // function that uses slice() and the other functions you've written to return the first half of the string.
-// var string = "hello" // => "he"
-// var first2 = string.slice(0,2)
-// console.log(first2)
+var string = "hello" // => "he"
+var first2 = string.slice(0,2)
+console.log(first2)
 
-// var string = "hello World" // => "hello"
-// var firstWord = string.slice(0,6)
-// console.log(firstWord)
+var string = "hello World" // => "hello"
+var firstWord = string.slice(0,6)
+console.log(firstWord)
 
 var string = "hello" // => "HEllo"
 var first2 = string.slice(0,2);
